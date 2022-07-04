445a39c95f6f5c6219e39453b64965c24b9347a6
Aesychylus <106658863+Aesychylus@users.noreply.github.com>
2022-07-04 03:14:11 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,91 +5,32 @@ namespace Project1
     class Program
     {
         static void Main(string[] args)
-        {   int[] hours = new int[3];
-            int fare_calculate;
-            
+        {
+            int[] hour = new int[3];
             Console.WriteLine("What type of vehicle was parked. Bike, Car or Bus");
             string a = Console.ReadLine();
 
-            for (int i = 0; i <= (hours.Length - 1) ; i++) 
+            for (int i = 0; i <= (hour.Length - 1); i++)
             {
                 Console.WriteLine("Please enter number of parking hours for each vehicle in a chronological order");
-                hours[i] = Convert.ToInt32(Console.ReadLine());
+                hour[i] = Convert.ToInt32(Console.ReadLine());
 
             }
+
+            Vehicle obj = new Vehicle();
 
             if (a== "bike")
             {
-                for (int m=0; m <= (hours.Length-1);m++)
-                {
-                    if (hours[m] <= 3)
-                    {
-                        fare_calculate = 2;
-                        Console.WriteLine("The parking fare for this person is" + " " + fare_calculate);
-
-                    }
-
-                    if (hours[m] > 3 && hours[m] <= 24)
-                    {
-                        fare_calculate = 2 + ((hours[m]-3) * 1/2);
-                        Console.WriteLine("The parking fare for this person is" + " " + fare_calculate);
-                    }
-
-                    if (hours[m] > 24)
-                    {
-                        fare_calculate = 10;
-                        Console.WriteLine("The parking fare for this person is" + " " + fare_calculate);
-                    }
-                }
+                obj.Bike(hour);
             }
             if (a == "car")
             {
-                for (int m = 0; m <= (hours.Length - 1); m++)
-                {
-                    if (hours[m] <= 3)
-                    {
-                        fare_calculate = 4;
-                        Console.WriteLine("The parking fare for this person is" + " " + fare_calculate);
-
-                    }
-
-                    if (hours[m] > 3 && hours[m] <= 24)
-                    {
-                        fare_calculate = 4 + ((hours[m] - 3) * 1);
-                        Console.WriteLine("The parking fare for this person is" + " " + fare_calculate);
-                    }
-
-                    if (hours[m] > 24)
-                    {
-                        fare_calculate = 20;
-                        Console.WriteLine("The parking fare for this person is" + " " + fare_calculate);
-                    }
-                }
+                obj.Car(hour);
             }
 
             if (a == "bus")
             {
-                for (int m = 0; m <= (hours.Length - 1); m++)
-                {
-                    if (hours[m] <= 3)
-                    {
-                        fare_calculate = 6;
-                        Console.WriteLine("The parking fare for this person is" + " " + "$" + fare_calculate);
-
-                    }
-
-                    if (hours[m] > 3 && hours[m] <= 24)
-                    {
-                        fare_calculate = 6 + ((hours[m] - 3) * 3 / 2);
-                        Console.WriteLine("The parking fare for this person is" + " " + "$" + fare_calculate);
-                    }
-
-                    if (hours[m] > 24)
-                    {
-                        fare_calculate = 30;
-                        Console.WriteLine("The parking fare for this person is" + " " + "$" + fare_calculate);
-                    }
-                }
+                obj.Bus(hour);
             }
         }
     }
